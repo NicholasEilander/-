965e6a56183a564e480cba69c83a397ba9a55a01
Nicholas Eilander(Nik) <90115179+NicholasEilander@users.noreply.github.com>
2025-09-25 00:33:34 +0300

    Add files via upload

---
+++ Pj3.cpp
@@ -0,0 +1,29 @@
+int main() {
+    //Ex 4
+    int A1 = 34;
+    int A2 = A1;
+
+    int B1 = 98;
+    int B2 = -B1;
+
+    int C1 = 345, C2 = -55;
+    int CSum = C1 + C2;
+
+    int D1 = 43, D2 = 87;
+    int DSub = D1 - D2;
+
+    int E1 = 7, E2 = 23;
+    int EMul = E1 * E2;
+
+    int F1 = 18, F2 = 6;
+    int FDiv = F1 / F2;
+
+    int G1 = 18, G2 = 5;
+    int GDiv2 = G1 / G2;
+
+    int H1 = 19, H2 = 4;
+    int HRest = H1 % H2;
+
+    float I1 = 81, I2 = 10;
+    float IDiv = I1 / I2;
+}

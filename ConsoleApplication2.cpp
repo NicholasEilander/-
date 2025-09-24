b9b16385ae4283fbd902b727f6ead49ef429a86f
Nicholas Eilander(Nik) <90115179+NicholasEilander@users.noreply.github.com>
2025-09-25 00:15:21 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+//приклад 4
+
+int main() {
+    const short CONST1 = 510;
+    short corpse1;
+    corpse1 = -85;
+    short corpse2 = 0x1c;
+
+    const int CONST2 = 0337752;
+    int corpse3;
+    corpse3 = 1024;
+    int corpse4 = 61.37;
+
+    const long CONST3 = 4015642735;
+    long cemetery1;
+    cemetery1 = 97.8;
+    long cemetery2 = 2768;
+
+    const unsigned int CONST4 = 438;
+    unsigned int stepler;
+    stepler = 74234;
+    unsigned int printer = -65;
+}

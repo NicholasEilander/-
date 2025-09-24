5e7ec45bfa50e657866cc95e281bcf73893ec360
Nicholas Eilander(Nik) <90115179+NicholasEilander@users.noreply.github.com>
2025-09-25 00:16:02 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,15 @@
+int main() {
+
+    const float CONST1 = 0.0000000002222;
+    float inabakumori;
+    inabakumori = -61879153264.89;
+    float autumn = 473.5e91;
+
+
+    double pupupu = -0.02e-300;
+    double wortex;
+    wortex = 365.1e258;
+
+    return 0;
+
+}
